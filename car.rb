21c8332bd02c5c3b8112a2e2588f8a5256ca68b0
MyaTse <myagirlnc@gmail.com>
2018-07-27 15:09:27 +0000

    automatically backed up by learn

--- car.rb
+++ car.rb
@@ -1,22 +1,4 @@
-class car 
-  def initialize car(model, make, year)
-    @carmodel = model
-    @carmake = make
-    @caryear = year
-  end
-  
-  def model
-    @carmodel
-  end
-  
-  def make
-    @carmake
-  end
-  
-  def year
-    @caryear
-  end
-end
+
 
 toyota = car.new("Camery", "Toyota", "2017")
 toyota.model
