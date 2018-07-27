763ac914a3be0a8a34f43b18f8728173b62b9f49
MyaTse <myagirlnc@gmail.com>
2018-07-27 15:04:14 +0000

    automatically backed up by learn

---
+++ car.rb
@@ -0,0 +1,19 @@
+class car 
+  def initialize car(model, make, year)
+    @carmodel = model
+    @carmake = make
+    @caryear = year
+  end
+  
+  def model
+    @carmodel
+  end
+  
+  def make
+    @carmake
+  end
+  
+  def year
+    @caryear
+  end
+end
